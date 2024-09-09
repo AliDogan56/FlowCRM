42b465a42477d39a18e31b63a0b0b383950492fa
ali.dogan <ali.dogan@techstep.io>
2024-09-09 22:16:40 +0200

    Adding-Auth-To-FE

--- ui/src/app/pages/user/list/UserListPage.tsx
+++ ui/src/app/pages/user/list/UserListPage.tsx
@@ -1,19 +1,28 @@
 import TableComponent from "../../../components/table/TableComponent";
 import {useEffect, useState} from "react";
 import {Pencil, Trash} from "react-bootstrap-icons"
-import authService from "../../../services/auth/AuthService";
-import {Button} from "react-bootstrap"
 import {CustomerUserModel, CustomerUserSearchDTO} from "../../../model/user/CustomerUserModel";
-import {UserRole} from "../../../model/user/AppUserModel";
 import userService from "../../../services/user/UserService";
 import {toast} from "react-toastify";
+import {Form, Button, Row, Col} from 'react-bootstrap';
+import {SortOrder} from "../../../model/base/SortOrderDTO";
+import authService from "../../../services/auth/AuthService";
+import {UserRole} from "../../../model/user/AppUserModel";
+
 
 const UserListPage = () => {
     const userType = new URL(window.location.href).pathname.toString().split("/")[2];
-    const userRole = authService.isSystemAdminOrOwner();
-
+    const isOwnerOrAdmin = authService.isSystemAdminOrOwner();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [region, setRegion] = useState('');
+    const [email, setEmail] = useState('');
     const [userData, setUserData] = useState<CustomerUserModel[]>([]);
     const [searchData, setSearchData] = useState<CustomerUserSearchDTO>();
+    const [sortOrder, setSortOrder] = useState<SortOrder>({fieldName: "id", orderProperty: "id", orderType: "ASC"});
+    const [page, setPage] = useState(0);
+    const [perPage, setPerPage] = useState(10);
+
 
     const customerColumns = [
         {
@@ -113,53 +122,10 @@ const UserListPage = () => {
 
     useEffect(() => {
         fetchData();
-    }, [userType]);
+    }, [userType, searchData]);
 
 
     const fetchData = async () => {
-        const fakeData: CustomerUserModel[] = [
-            {
-                id: 1,
-                createdAt: "2024-09-09T12:00:00Z",
-                username: "john.doe",
-                role: UserRole.Customer,
-                firstName: "John",
-                lastName: "Doe",
-                region: "North",
-                email: "john.doe@example.com"
-            },
-            {
-                id: 2,
-                createdAt: "2024-09-10T14:30:00Z",
-                username: "jane.smith",
-                role: UserRole.Customer,
-                firstName: "Jane",
-                lastName: "Smith",
-                region: "South",
-                email: "jane.smith@example.com"
-            },
-            {
-                id: 3,
-                createdAt: "2024-09-11T09:15:00Z",
-                username: "alice.jones",
-                role: UserRole.Customer,
-                firstName: "Alice",
-                lastName: "Jones",
-                region: "East",
-                email: "alice.jones@example.com"
-            },
-            {
-                id: 4,
-                createdAt: "2024-09-12T17:45:00Z",
-                username: "bob.brown",
-                role: UserRole.Customer,
-                firstName: "Bob",
-                lastName: "Brown",
-                region: "West",
-                email: "bob.brown@example.com"
-            }
-        ];
-        setUserData(fakeData);
         userService.search(userType, searchData).then(response => {
                 setUserData(response.data);
             },
@@ -169,12 +135,87 @@ const UserListPage = () => {
     };
 
 
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const newSearchData: CustomerUserSearchDTO = {
+            firstName,
+            lastName,
+            region,
+            email,
+            orderProperty: sortOrder?.orderProperty,
+            orderType: sortOrder?.orderType,
+            page: page,
+            pageSize: perPage
+        };
+
+        setSearchData(newSearchData);
+    };
+
     return (
         <div className="container mt-4">
-            <h1>User Data Table</h1>
+            <h1>Customer Users</h1>
+            {userType === 'customer' &&
+                <Form onSubmit={handleSubmit} className="p-3">
+                    <Row className="mb-3">
+                        <Col md={6}>
+                            <Form.Group controlId="formFirstName">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter first name"
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={6}>
+                            <Form.Group controlId="formLastName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter last name"
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className="mb-3">
+                        <Col md={6}>
+                            <Form.Group controlId="formRegion">
+                                <Form.Label>Region</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter region"
+                                    value={region}
+                                    onChange={(e) => setRegion(e.target.value)}
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={6}>
+                            <Form.Group controlId="formEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <div className="text-end">
+                        <Button variant="primary" type="submit">
+                            Search
+                        </Button>
+                    </div>
+                </Form>
+            }
+
             <TableComponent columns={userType === 'customer' ? customerColumns : systemOwnerOrAdminColumns}
                             data={userData}
-                            showActions={true}/>
+                            showActions={( !(userType === "systemowner") && (isOwnerOrAdmin))}/>
         </div>
     );
 }
